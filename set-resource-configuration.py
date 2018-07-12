cb93c57f6cdfbc5ff2a23a71b2b3d732c68955fb
Christian Dröge <mail@cdroege.de>
2018-07-12 23:13:56 +0200

    Too many modifications

---
+++ set-resource-configuration.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+import ruamel.yaml
+import io
+
+def main(filepath, vcpus, mem, storage):
+
+    with open(filepath) as stream:
+        try:
+            yaml_dict = (ruamel.yaml.load(stream, Loader=ruamel.yaml.RoundTripLoader, preserve_quotes=True))
+            yaml_dict["virtual_deployment_units"][0]["resource_requirements"]["cpu"]["vcpus"] = int(vcpus)
+            yaml_dict["virtual_deployment_units"][0]["resource_requirements"]["memory"]["size"] = int(mem)
+            yaml_dict["virtual_deployment_units"][0]["resource_requirements"]["storage"]["size"] = int(storage)
+            yaml_dict["virtual_deployment_units"][0]["resource_requirements"]["memory"]["size_unit"] = "MB"
+            yaml_dict["virtual_deployment_units"][0]["resource_requirements"]["storage"]["size_unit"] = "MB"
+            with io.open(filepath + "2", 'w', encoding='utf8') as outfile:
+                ruamel.yaml.dump(yaml_dict, outfile, Dumper=ruamel.yaml.RoundTripDumper, allow_unicode=True)
+            return 0
+        except ruamel.yaml.YAMLError as exc:
+            print(exc)
+            return 1
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]))
+
+
